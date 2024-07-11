22d83c5d55a6f387acb7a6cd5686fe05c242bfe0
teresiawairimu <teresiawairimu06@gmail.com>
2024-07-11 08:43:56 -0700

    task 2

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            print(f"DEBUG: Calling storage.new() for new {self.__Class__.__name__} object")
+            """storage.new(self)"""
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -27,6 +28,7 @@ class BaseModel:
                 self.created_at = datetime.now()
             if 'updated_at' not in kwargs:
                 self.updated_at = datetime.now()
+        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
