aa54a77246511a31352044c8c95b77b32daf228e
teresiawairimu <teresiawairimu06@gmail.com>
2024-07-11 11:37:53 -0700

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,4 @@
-/*
-Prepares a MYSQL server for the AirBnB clone project:
- Creates a database hbnb_dev_db
- Creates a new user hbnb_dev (in localhost)
- Sets the password of hbnb_dev
- Grants privileges to hbnb_dev on hbnb_dev_db
-*/
+--Prepares a MYSQL server for the AirBnB clone project:
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
