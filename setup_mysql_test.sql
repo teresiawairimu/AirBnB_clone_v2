33572ff1600bf845f33aa56c6fb25ba1f6321d92
teresiawairimu <teresiawairimu06@gmail.com>
2024-07-11 12:04:46 -0700

    task 3

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
---creates database hbnb_test_db
+-- creates database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
