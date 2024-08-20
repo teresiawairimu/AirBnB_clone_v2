32c21d6c242bb958049b3d2294ef8689b0faf46b
teresiawairimu <teresiawairimu06@gmail.com>
2024-08-20 10:37:26 -0700

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,15 +10,18 @@ def hello_hbnb():
     """Display Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route('hbnb', strict_slashes=False)
 def hbnb():
     """Displays HBNB"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Replace underscores with spaces"""
     return f"C {text.replace('_', ' ')}"
+
 
 if __name__ = "__main__":
     app.run(host="0.0.0.0", port=5000)
