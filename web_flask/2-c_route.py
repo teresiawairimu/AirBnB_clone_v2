0c1d2beb3d13aebe6aa7a073457e4e754fe2f765
teresiawairimu <teresiawairimu06@gmail.com>
2024-08-20 17:43:59 +0000

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,19 +7,27 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Display Hello HBNB!"""
+    """Route to display Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('hbnb', strict_slashes=False)
 def hbnb():
-    """Displays HBNB"""
+    """Route to displays HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """Replace underscores with spaces"""
+    """Route to display C followed by the value of the text
+       Replace underscores with spaces
+
+       Args:
+           text (str): The text to display after C
+       Returns:
+           str: The formatted string with C followed the text.
+
+    """
     return f"C {text.replace('_', ' ')}"
 
 
