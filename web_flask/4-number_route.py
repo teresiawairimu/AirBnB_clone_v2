b761c762f6082941d70343254a17749514aeadf0
teresiawairimu <teresiawairimu06@gmail.com>
2024-08-20 12:19:15 -0700

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Route to display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route to displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Route to display C followed by the value of the text
+       Replace underscores with spaces
+
+       Args:
+           text (str): The text to display after C
+       Returns:
+           str: The formatted string with C followed the text.
+
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """Route displays Python followed by the value of the text variable
+       underscores are replaces with spaces
+
+    Args:
+       text (str): The text displayed after Python
+    Returns:
+       str: The formatted string with Python followed by text
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Route to display n is a number only if n is an integer
+    Args:
+        n (int): The number to display
+    Returns:
+        str: The formatted string with n is a number
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
