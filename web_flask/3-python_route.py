5bed48df62850f47a9f496b5535f9c1e4096023d
teresiawairimu <teresiawairimu06@gmail.com>
2024-08-20 12:09:53 -0700

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -30,6 +30,7 @@ def c_text(text):
     """
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text):
